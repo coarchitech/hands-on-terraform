d4ccffd5856840d6f36b0c010b70a38b55aa238d
Jeisson Osorio <jeisson.osoriob@gmail.com>
2021-06-23 08:56:54 -0500

    ADD: Se carga definicion de modulo para function en Azure

---
+++ coarchitech-iac/azure/app/hello-world/index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const responseMessage =  "Hello Coarchitech Community from azure"
+        
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage,
+        headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, POST'}           
+        
+    };
+
+}
